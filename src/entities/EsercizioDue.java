abaed60255df78996fc1fbf50b308bf6d8e94d02
itsemanuela <notemanu152@gmail.com>
2026-06-09 15:00:57 +0200

    Prima parte esercizioDue

--- src/entities/EsercizioDue.java
+++ src/entities/EsercizioDue.java
@@ -1,4 +1,20 @@
 package entities;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 public class EsercizioDue {
+
+    public static List<Integer> randomNum(int numero){
+        List<Integer> lista = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < numero; i++) {
+          int numeroRandom = random.nextInt(101);
+          lista.add(numeroRandom);
+        } return  lista;
+    }
+
+
+
 }
--- src/entities/MainDue.java
+++ src/entities/MainDue.java
@@ -1,4 +1,13 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainDue {
+    public static void main(String[] args){
+        List<Integer> lista = EsercizioDue.randomNum(5);
+        System.out.println("Prima lista" + " " +  lista);
+
+
+    }
 }
